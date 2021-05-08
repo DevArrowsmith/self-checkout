3569c3c82d521901e563c41672ba8d96fd7ea876
devarrowsmith <devarrowsmith@gmail.com>
2021-05-08 11:13:00 +0100

    Create test: selfCheckout: adds an item to a basket array.

--- test/selfCheckout.test.js
+++ test/selfCheckout.test.js
@@ -3,7 +3,19 @@ const {orange, pineapple, kiwi, apple, banana, catalogue} = require("../data/sel
 const { selfCheckout } = require ("../src/selfCheckout")
 
 describe("selfCheckout", () => {
+
+    afterEach(() => {
+        selfCheckout.basket = [];
+    });
+
     test("scans an item's barcode and retrieves that item's details", () => {
         expect(selfCheckout.scanBarcode(765)).toEqual(kiwi);
-    })
-})
+    });
+
+    test("adds an item to a basket array", () => {
+        selfCheckout.addToBasket(kiwi);
+        expect(selfCheckout.basket).toEqual([kiwi]);
+        selfCheckout.addToBasket(pineapple);
+        expect(selfCheckout.basket).toEqual([kiwi, pineapple]);
+    });
+});
